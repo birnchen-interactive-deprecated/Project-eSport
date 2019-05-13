9fd485324a3af6adbf98f011eae318e0450e9d23
Birnchen <47638797+Birnchen@users.noreply.github.com>
2019-05-13 13:16:34 +0200

    dump

--- modules/user/views/user/addGameId.php
+++ modules/user/views/user/addGameId.php
@@ -1,1 +1,18 @@
-addGameId.php
+<?php
+
+/* @var $this yii\web\View *
+/* @var $form yii\bootstrap\ActiveForm
+ * @var $id int
+ * @var $gamesList array
+ * @var $platformList array
+/* @var $model app\modules\platformgames\models\formModel\UserGameForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+\app\modules\user\assets\UserAsset::register($this);
+
+?>
+
+<div class="site-addGame">
+</div>
