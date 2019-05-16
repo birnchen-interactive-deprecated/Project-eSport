e052771f0e5b59709d093c722bbfad60c5dc039a
Birnchen <birnchen@birnchen-interactive.de>
2019-05-16 22:08:06 +0200

    footer

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -71,7 +71,7 @@ class SiteController extends BaseController
     {
         $isGuest = (Yii::$app->user->isGuest) ? false : true;
 
-         $twitterImg = Html::img(Yii::$app->HelperClass->checkImage('/images/socialMedia/', 'Twitter_Logo_Blue') . 'webp', ['height' => '49px', 'alt'=> 'twitter image', 'aria-label' => 'twitter image']);
+         $twitterImg = Html::img('..' . Yii::$app->HelperClass->checkImage('/images/socialMedia/', 'Twitter_Logo_Blue') . 'webp', ['height' => '49px', 'alt'=> 'twitter image', 'aria-label' => 'twitter image']);
 
          //$twitterLink = Html::a($twitterImg, 'https://twitter.com/esport_project', ['target' => '_blank', 'rel' =>'noopener', 'aria-label' => 'Follow us on twitter', 'label' => 'twitter']);
 
