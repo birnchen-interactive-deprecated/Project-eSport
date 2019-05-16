1a155d3914b37a2f1f12743a03215f0e0ac270d4
Birnchen <birnchen@birnchen-interactive.de>
2019-05-16 22:33:01 +0200

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,10 +17,10 @@ $visible = (Yii::$app->user->isGuest) ? false : true;
 $userID = (Yii::$app->user->isGuest) ? -1 : Yii::$app->user->identity->getId();
 
 /** Footer Images */
-$twitterImg = Html::img('../images/socialMedia/Twitter_Logo_Blue.png', ['height' => '49px', 'alt'=> 'twitter.png', 'aria-label' => 'twitter.png']);
+$twitterImg = Html::img('../images/socialMedia/Twitter_Logo_Blue.webp', ['height' => '49px', 'alt'=> 'twitter', 'aria-label' => 'twitter', 'onerror' => 'this.src=../images/socialMedia/Twitter_Logo_Blue.png']);
 $twitterLink = Html::a($twitterImg, 'https://twitter.com/esport_project', ['target' => '_blank', 'rel' =>'noopener', 'aria-label' => 'Follow us on twitter', 'label' => 'twitter']);
 
-$discordImg = Html::img('../images/socialMedia/Discord-Logo-White.png', ['height' => '49px', 'alt'=> 'discord.png', 'aria-label' => 'discord.png', 'style' => 'padding: 5px 0; ']);
+$discordImg = Html::img('../images/socialMedia/Discord-Logo-White.webp', ['height' => '49px', 'alt'=> 'discord', 'aria-label' => 'discord', 'style' => 'padding: 5px 0; ']);
 $discordLink = Html::a($discordImg, 'https://discord.gg/rk3qd9U', ['target' => '_blank', 'rel' =>'noopener', 'aria-label' => 'Join our Discord Server']);
 
 /** some stuff */
