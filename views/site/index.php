3fa877885f32c78c3193caa2a4df34d3ee5c61b6
CaptFreezer <freezerxxl@gmx.net>
2019-05-16 23:22:36 +0200

    startseite links und rechts platz eingefuegt

--- views/site/index.php
+++ views/site/index.php
@@ -12,7 +12,12 @@ Yii::$app->MetaClass->writeMetaIndex($this, $this->title);
 
 <div class="site-index">
     <main>
-        <div class="welcome">
+
+        <div class="col-lg-3">
+            <!-- Platzhalter für BIRNCHEN links -->
+        </div>
+
+        <div class="col-lg-6 welcome">
             Willkommen auf unserer Turnier Website.<br>
             Besuch uns doch bei Fragen auf unserem <a class="dclink" href="https://discord.gg/rk3qd9U">Discord</a> Server.
             <br>
@@ -20,5 +25,10 @@ Yii::$app->MetaClass->writeMetaIndex($this, $this->title);
             Noch kein Team angelegt??? <br>
             Melde dich auf <a class="dclink" href="https://discord.gg/rk3qd9U">Discord</a> bei <b><i><u>Birnchen | Pierre#5366</u></i></b>.
         </div>
+
+        <div class="col-lg-3">
+            <!-- Platzhalter für BIRNCHEN rechts -->
+        </div>
+
     </main>
 </div>
