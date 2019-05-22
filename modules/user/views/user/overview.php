2d3915e872add473c228b7a664d43782cfb6882f
Philipp Jarnig <philipp.jarnig@gwp.dpd.at>
2019-05-22 14:30:46 +0200

    Style für Birnchen

--- modules/user/views/user/overview.php
+++ modules/user/views/user/overview.php
@@ -16,7 +16,16 @@ $this->title = 'Team Details';
             $username = $user->getUsername();
             $userId = $user->getId();
         ?>
-        <div class="col-lg-4"><?= Html::a($username, ['/user/details', 'id' => $userId]); ?></div>
+        <div class="playerRow clearfix">
+        	<!-- summe Y aller col-lg-X darf 12 nicht ueberschreiten, sonst bricht es um -->
+        	<div class="col-lg-1"><!-- User ID ??? --></div>
+	        <div class="col-lg-2"><?= Html::a($username, ['/user/details', 'id' => $userId]); ?></div>
+	        <div class="col-lg-2"><!-- Main Teams --></div>
+	        <div class="col-lg-2"><!-- Sub Teams --></div>
+	        <div class="col-lg-2"><!-- Container 1 --></div>
+	        <div class="col-lg-2"><!-- Container 2 --></div>
+	        <div class="col-lg-1"><!-- Container 3 --></div>
+		</div>
 	<?php endforeach; ?>
 
 	<?php
