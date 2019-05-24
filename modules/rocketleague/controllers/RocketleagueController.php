95af2ce75c490bc76ff855f6422e7e6f32fede48
Philipp Jarnig <philipp.jarnig@gwp.dpd.at>
2019-05-24 10:32:24 +0200

    bugfixing

--- modules/rocketleague/controllers/RocketleagueController.php
+++ modules/rocketleague/controllers/RocketleagueController.php
@@ -262,14 +262,14 @@ class RocketleagueController extends BaseController
             return $this->redirect('tournament-details?id=' . $tournament_id);
         }
 
-        $bracket = Bracket::getById($bracketId);
-        if ($bracket->tournament_id !== $tournament_id) {
-            Alert::addError('Das Bracket ist nicht in diesem Turnier. ' . var_export($bracketId, true));
+        if ($winner !== 1 && $winner !== 2) {
+            Alert::addError('Der Sieger muss gesetzt sein. ' . var_export($winner, true));
             return $this->redirect('tournament-details?id=' . $tournament_id);
         }
 
-        if ($winner !== 1 && $winner !== 2) {
-            Alert::addError('Der Sieger muss gesetzt sein. ' . var_export($winner, true));
+        $bracket = Bracket::getById($bracketId);
+        if ($bracket->tournament_id !== $tournament_id) {
+            Alert::addError('Das Bracket ist nicht in diesem Turnier. ' . var_export($bracketId, true));
             return $this->redirect('tournament-details?id=' . $tournament_id);
         }
 
