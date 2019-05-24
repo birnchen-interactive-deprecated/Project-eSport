3d4f9ea24c59dd102bea8f8966c0a7f7c8539092
CaptFreezer <freezerxxl@gmx.net>
2019-05-24 21:31:05 +0200

    Optik für Brackets

--- modules/rocketleague/views/rocketleague/tournamentDetails.php
+++ modules/rocketleague/views/rocketleague/tournamentDetails.php
@@ -293,6 +293,18 @@ $this->title = 'Turnier Details';
 
                         <?php foreach ($roundBrackets as $bracketKey => $bracket): ?>
                             <?php
+                            
+                                $bracketFinished = $bracket->checkisFinished();
+                                $class1 = '';
+                                $class2 = '';
+                                if ($bracketFinished === 1) {
+                                    $class1 = 'winner';
+                                    $class2 = 'looser';
+                                } else if ($bracketFinished === 2) {
+                                    $class1 = 'looser';
+                                    $class2 = 'winner';
+                                }
+
                                 $bracketEncounter = $bracket->getEncounterId();
                                 $bracketParticipants = $bracket->getParticipants();
                                 $bracketParticipants[0] = ($bracketParticipants[0] === NULL) ? 'FREILOS' : $bracketParticipants[0];
@@ -308,8 +320,8 @@ $this->title = 'Turnier Details';
 
                             <span class="bracketEncounter">Bracket <?= $bracketEncounter; ?> | Gamename and Password: R<?= $round . str_pad($bracketEncounter, 2, '0', STR_PAD_LEFT); ?></span>
                             <div class="bracket">
-                                <div class="bracketParticipant"><?= $participant1; ?></div>
-                                <div class="bracketParticipant"><?= $participant2; ?></div>
+                                <div class="bracketParticipant <?= $class1; ?>"><?= $participant1; ?></div>
+                                <div class="bracketParticipant <?= $class2; ?>"><?= $participant2; ?></div>
                             </div>
 
                         <?php endforeach; ?>
