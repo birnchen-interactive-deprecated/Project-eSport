9e08e2eab2d5b6340bb93a028dc4ae5ec52896b2
Birnchen <47638797+Birnchen@users.noreply.github.com>
2019-05-25 10:26:59 +0200

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -16,7 +16,28 @@ Yii::$app->MetaClass->writeMetaIndex($this, $this->title);
     <main>
 
         <div class="col-lg-9 welcome">
-            
+            <div class="newsContainer">
+                <div class="containerHeader">
+                    <span class="headerKategorie"><img src="../images/PeSpLogos/pesp.png">Website</span>
+                    <span class="headerTitle">Team up and get Ready</span>
+                    <span class="headerDate">25.05.2019</span>
+                </div>
+                <div class="containerBody clearfix">
+                    <div class="containerImage col-lg-3"><img src="../images/PeSpLogos/pesp.png" alt="" style="width: 100%; padding-top: 0px;"></div>
+                    <div class="containerText col-lg-9">
+                        It's done!!!<br>                  
+                        Players and teams can finally manage themselves.<br>
+                        <br>
+                        Players:<br>
+                        Go to your account and click on the button next to Main Team. Congratulations you have created your first own team with us.<br>
+                        <br>
+                        Teams:<br>
+                        Go to your Main Team (via Account) and click on the button next to Sub Teams. Congratulations you have created your first own team with us.
+                        <br>
+                    </div>
+                    <div class="containerAuthor">Birnchen</div>
+                </div>
+            </div>
 
             <div class="newsContainer">
                 <div class="containerHeader">
@@ -27,12 +48,12 @@ Yii::$app->MetaClass->writeMetaIndex($this, $this->title);
                 <div class="containerBody clearfix">
                     <div class="containerImage col-lg-3"><img src="https://pbs.twimg.com/media/D7Xrs37UYAAt_xi?format=jpg&name=small" alt="" style="width: 100%;     padding-top: 33px;"></div>
                     <div class="containerText col-lg-9">
-                    Congratulations to <?= Html::a('xer02rl', ['/user/details', 'id' => '156']); ?> to this amazing win Today in 1v1.
-                    He beated up 15 other participating Players on the first Day of the new Season.<br>
-                    After a long Night he gets the First 20 Points for the 600€ Season Price Pool.
-                    <br><br>
-                    Second Place: <?= Html::a('Dongiii', ['/user/details', 'id' => '162']); ?> with 17 Points<br>
-                    Third Place: <?= Html::a('NoAvian', ['/user/details', 'id' => '38']); ?> with 15 Points<br>
+                        Congratulations to <?= Html::a('xer02rl', ['/user/details', 'id' => '156']); ?> to this amazing win Today in 1v1.<br>
+                        He beat 15 other participating Players on the first Day of the new Season.<br>
+                        After a long Night he gets the First 20 Points for the 600€ Season Price Pool.
+                        <br><br>
+                        Second Place: <?= Html::a('Dongiii', ['/user/details', 'id' => '162']); ?> with 17 Points<br>
+                        Third Place: <?= Html::a('NoAvian', ['/user/details', 'id' => '38']); ?> with 15 Points<br>
                     </div>
                     <div class="containerAuthor">Birnchen</div>
                 </div>
