6bec21ed8460cafafc906cd73dfe17200e19cf9d
Philipp Jarnig <philipp.jarnig@gwp.dpd.at>
2019-05-27 14:46:18 +0200

    Bugfix

--- modules/rocketleague/controllers/RocketleagueController.php
+++ modules/rocketleague/controllers/RocketleagueController.php
@@ -503,7 +503,7 @@ class RocketleagueController extends BaseController
 
             $id++;
 
-            if (false !== $bracket2 && !$bracket1->getIsWinnerBracket()) {
+            if (false !== $bracket1 && !$bracket1->getIsWinnerBracket()) {
                 break;
             }
 
