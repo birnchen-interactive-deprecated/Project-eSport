3ba435c03abf0755b2399754f7b45419cdf68298
Birnchen <47638797+Birnchen@users.noreply.github.com>
2019-05-31 14:31:40 +0200

    Update tournamentDetails.php

--- modules/rocketleague/views/rocketleague/tournamentDetails.php
+++ modules/rocketleague/views/rocketleague/tournamentDetails.php
@@ -334,7 +334,7 @@ $this->title = \app\modules\rocketleague\Module::t('details', 'tournamentdetails
                                     <div class="bracketParticipant <?= $class2; ?>">
                                         <?= $participant2; ?>
                                         <!-- Captains/Deputy Area -->
-                                        <?php if($bracket->isManageable($user->getId())) : ?>
+                                        <?php if($user != null && $bracket->isManageable($user->getId())) : ?>
                                             <div class="takeWinner" style="float: right;">
                                                     <?php echo Html::a('',
                                                         [
