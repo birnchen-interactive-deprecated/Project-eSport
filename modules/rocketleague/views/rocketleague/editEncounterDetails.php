d10e9b72bc0321f963087f84552baa3e0358f25b
Philipp Jarnig <philipp.jarnig@gwp.dpd.at>
2019-06-03 15:48:32 +0200

    bugfix

--- modules/rocketleague/views/rocketleague/editEncounterDetails.php
+++ modules/rocketleague/views/rocketleague/editEncounterDetails.php
@@ -13,6 +13,8 @@ use yii\bootstrap\ActiveForm;
 use app\modules\user\models\User;
 use app\modules\teams\models\SubTeam;
 
+app\modules\rocketleague\assets\rocketleagueAsset::register($this);
+
 $imgLeft  = ($player_left  instanceof User) ? '/images/userAvatar/' . $player_left->id  : '/images/teams/subTeams/' . $player_left->id;
 $imgRight = ($player_right instanceof User) ? '/images/userAvatar/' . $player_right->id : '/images/teams/subTeams/' . $player_right->id;
 
