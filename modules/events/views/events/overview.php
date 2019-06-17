9cf55e307cc78605a1043cbede7fc049c9233e0c
Birnchen <47638797+Birnchen@users.noreply.github.com>
2019-06-17 21:19:13 +0200

    Update overview.php

--- modules/events/views/events/overview.php
+++ modules/events/views/events/overview.php
@@ -10,18 +10,22 @@ $pespLink = Html::a($pespImg, 'https://project-esport.gg', ['target' => '_blank'
 $dvisionImg = Html::img('../images/PeSpLogos/DVision.webp', ['height' => '50px', 'alt'=> 'discord', 'aria-label' => 'discord', 'onerror' => 'this.src=\'../images/PeSpLogos/DVision.png\'', 'style' => 'padding: 5px 0; ']);
 $dvisionLink = Html::a($dvisionImg, 'https://www.dvision-diner.com', ['target' => '_blank', 'rel' =>'noopener', 'aria-label' => 'eSports Diner']);
 
+$holdiImg = Html::img('https://media.discordapp.net/attachments/509042339650600960/580745164486737920/my_logo.jpg?width=492&height=492',['height' => '50px', 'alt'=> 'twitter', 'aria-label' => 'twitter']);
+$holdiLink = Html::a($holdiImg, 'https://discord.gg/gqpfaQw', ['target' => '_blank', 'rel' => 'noopener', 'aria-label' => 'HoldieMoldie Discord']);
+
 ?>
 
 <div class="site-events-overview">
 	<center>
     	<div><h1><b>Köln 23.08.2019</b></h1></div>
-    	<br>
+    	<h2><b>Einlass ab 18:00 Uhr</b></h2>
     	<br>
     	<br>
     	<div><h2><b>Save the Date!!!</b></h2></div>
     	<br>
     	<br>
     	<br>
-    	<span><?= $pespLink; ?></span><span><?= $dvisionLink; ?>
+    	<span><?= $pespLink; ?></span><span><?= $dvisionLink; ?><span><?= $holdiLink; ?>
 	</center>
+    <script type="text/javascript" src="https://satellite.booking-time.com/?js=8de288eeadc29aab"></script>
 </div>
